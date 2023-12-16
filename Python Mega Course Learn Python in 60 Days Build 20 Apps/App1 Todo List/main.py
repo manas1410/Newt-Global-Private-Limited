698b0ce1ea186e291e487f1a0cb9acc5f6c0a3d3
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-16 13:17:27 +0530

    todos

--- Python Mega Course Learn Python in 60 Days Build 20 Apps/App1 Todo List/main.py
+++ Python Mega Course Learn Python in 60 Days Build 20 Apps/App1 Todo List/main.py
@@ -1,2 +1,10 @@
-user_text = input("Enter a todo: ")
-print(user_text)
+promt = "Enter a todo: "
+todo1 = input(promt)
+todo2 = input(promt)
+todo3 = input(promt)
+
+todos = [todo1, todo2, todo3, "Hello"]
+print(todos)
+
+print(type(todo1))
+print(type(todos))
