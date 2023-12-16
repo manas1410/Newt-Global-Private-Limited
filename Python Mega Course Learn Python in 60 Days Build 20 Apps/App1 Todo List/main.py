55be88da14ddfb1e7d8e92a96b605ba073bff7cd
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-16 13:38:12 +0530

    while True

--- Python Mega Course Learn Python in 60 Days Build 20 Apps/App1 Todo List/main.py
+++ Python Mega Course Learn Python in 60 Days Build 20 Apps/App1 Todo List/main.py
@@ -1,10 +1,6 @@
-promt = "Enter a todo: "
-todo1 = input(promt)
-todo2 = input(promt)
-todo3 = input(promt)
+user_promt = "Enter a todo: "
 
-todos = [todo1, todo2, todo3, "Hello"]
-print(todos)
-
-print(type(todo1))
-print(type(todos))
+while True:
+    todo = input(user_promt)
+    print(todo)
+    print("Next...")
