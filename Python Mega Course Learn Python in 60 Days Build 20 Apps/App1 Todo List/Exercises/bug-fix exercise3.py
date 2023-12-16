38f8c354fa065bc3e8809cfead6126e2f51fd641
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-16 17:03:56 +0530

    bug fix exercise3

---
+++ Python Mega Course Learn Python in 60 Days Build 20 Apps/App1 Todo List/Exercises/bug-fix exercise3.py
@@ -0,0 +1,13 @@
+#1
+buttons = ["cancel", "reply", "submit"]
+for i in buttons:
+    print(i.capitalize())
+
+#2
+buttons = ["cancel", "reply", "submit"]
+for i in buttons:
+    print(i.capitalize())
+
+#3
+for item in ['sandals', "glasses", "trousers"]:
+    print(item.capitalize())
