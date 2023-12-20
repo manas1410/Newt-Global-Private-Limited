2eede85c130d9102fcf4bdd7096ba533eaa17869
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-20 21:48:23 +0530

    Day 25 - Initial commit

--- Python Mega Course Learn Python in 60 Days Build 20 Apps/app3-pdf_template/Student Project: Lined Pdf/main.py
+++ Python Mega Course Learn Python in 60 Days Build 20 Apps/app3-pdf_template/Student Project: Lined Pdf/main.py
@@ -13,7 +13,7 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
-         ln=1)
+             ln=1)
     for y in range(20, 298, 10):
         pdf.line(10, y, 200, y)
 
@@ -22,7 +22,6 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
-
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
