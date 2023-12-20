d3a2b7b2e28d22b0e29ffc244840fdfcff8d6531
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-20 22:25:11 +0530

    Day 26: Add an invoice date

--- Python Mega Course Learn Python in 60 Days Build 20 Apps/app4-invoice-generation/main.py
+++ Python Mega Course Learn Python in 60 Days Build 20 Apps/app4-invoice-generation/main.py
@@ -1,25 +1,25 @@
-from fpdf import FPDF
+import pandas as pd
 import glob
+from fpdf import FPDF
 from pathlib import Path
 
-# Create a list of text filepaths
-filepaths = glob.glob("files/*.txt")
-# Create one PDF file
-pdf = FPDF(orientation="P", unit="mm", format="A4")
+filepaths = glob.glob("invoices/*.xlsx")
 
-# Go through each text file
 for filepath in filepaths:
-    # Add a page to the PDF document for each text file
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
 
-    # Get the filename without the extension
-    # and convert it to title case (e.g. Cat)
     filename = Path(filepath).stem
-    name = filename.title()
+    invoice_nr, date = filename.split("-")
 
-    # Add the name to the PDF
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=name, ln=1)
+    pdf.cell(w=50, h=8, txt=f"Invoice nr {invoice_nr}", ln=1)
 
-# Produce the PDF
-pdf.output("output.pdf")
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr {date}")
+
+    pdf.output(f"PDFs/{filename}.pdf")
+
+
