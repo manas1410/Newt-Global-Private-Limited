906a8a69e759dfe12f351f67047dc47cc7e76818
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-20 22:01:15 +0530

    Day 25: Student Project: From Text Files to PDF

--- Python Mega Course Learn Python in 60 Days Build 20 Apps/app4-invoice-generation/main.py
+++ Python Mega Course Learn Python in 60 Days Build 20 Apps/app4-invoice-generation/main.py
@@ -1,18 +1,25 @@
-import pandas as pd
-import glob
 from fpdf import FPDF
+import glob
 from pathlib import Path
 
-filepaths = glob.glob("invoices/*.xlsx")
+# Create a list of text filepaths
+filepaths = glob.glob("files/*.txt")
+# Create one PDF file
+pdf = FPDF(orientation="P", unit="mm", format="A4")
 
+# Go through each text file
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    # Add a page to the PDF document for each text file
     pdf.add_page()
+
+    # Get the filename without the extension
+    # and convert it to title case (e.g. Cat)
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    name = filename.title()
+
+    # Add the name to the PDF
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr {invoice_nr}")
-    pdf.output(f"PDFs/{filename}.pdf")
+    pdf.cell(w=50, h=8, txt=name, ln=1)
 
-
+# Produce the PDF
+pdf.output("output.pdf")
