d0e9f9b1687270335f2038d92d758a8769064fb7
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-20 14:01:25 +0530

    Multiple Pages

--- Python Mega Course Learn Python in 60 Days Build 20 Apps/web_app1/web.py
+++ Python Mega Course Learn Python in 60 Days Build 20 Apps/web_app1/web.py
@@ -2,17 +2,24 @@ import streamlit as st
 import functions
 
 
+todos = functions.get_todos()
+
+st.set_page_config(layout="wide")
 def add_todo():
     todo = st.session_state["new_todo"]
     todos.append(todo + "\n")
     functions.write_todos(todos)
 
-todos = functions.get_todos()
-
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <b>productivity.</b>",
+         unsafe_allow_html=True)
+
+
+st.text_input(label=" ", placeholder="Add new todo...",
+              on_change= add_todo, key="new_todo")
+
 
 
 for index, todo in enumerate(todos):
@@ -23,6 +30,3 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-
-st.text_input(label=" ", placeholder="Add new todo...",
-              on_change= add_todo, key="new_todo")
