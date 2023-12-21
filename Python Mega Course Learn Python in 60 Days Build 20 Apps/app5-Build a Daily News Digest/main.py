6608c3e3ed89a559212f44cbd433cddea2984f4e
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-21 14:50:05 +0530

    Add links, Specify language, limit and news, add subject , control categoty

--- Python Mega Course Learn Python in 60 Days Build 20 Apps/app5-Build a Daily News Digest/main.py
+++ Python Mega Course Learn Python in 60 Days Build 20 Apps/app5-Build a Daily News Digest/main.py
@@ -1,9 +1,12 @@
 import requests
+from send_email import send_email
 
-api_key = "db6b500738ce49559a58ca799f377147"
-url = (("https://newsapi.org/v2/everything?q=tesla&"\
-       "from=2023-11-21&sortBy=publishedAt&apiKey=") +
-       api_key)
+
+topic = "tesla"
+api_key = "890603a55bfa47048e4490069ebee18c"
+url = f"https://newsapi.org/v2/everything?q={topic}&" \
+      "sortBy=publishedAt&apiKey=" \
+      "890603a55bfa47048e4490069ebee18c&language=en"
 
 # Make request
 request = requests.get(url)
@@ -12,6 +15,12 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+body = ""
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+        body = "Subject: Today's news" + body + article["title"] + "\n" + \
+        str(article["description"]) + \
+        "\n" + article["url"]  +2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
