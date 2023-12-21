f7566ae25a304eaf87995c162bd80e7d28725929
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-21 14:41:36 +0530

    Access the news titles and  descriptions

---
+++ Python Mega Course Learn Python in 60 Days Build 20 Apps/app5-Build a Daily News Digest/README.md
@@ -0,0 +1,2 @@
+# What is this project?
+THis app accesses news about a particular topic and sends them by email.
--- Python Mega Course Learn Python in 60 Days Build 20 Apps/app5-Build a Daily News Digest/main.py
+++ Python Mega Course Learn Python in 60 Days Build 20 Apps/app5-Build a Daily News Digest/main.py
@@ -5,6 +5,13 @@ url = (("https://newsapi.org/v2/everything?q=tesla&"\
        "from=2023-11-21&sortBy=publishedAt&apiKey=") +
        api_key)
 
+# Make request
 request = requests.get(url)
-content = request.text
-print(content)
+
+# Get a dictionary with data
+content = request.json()
+
+# Access the article titles and description
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
