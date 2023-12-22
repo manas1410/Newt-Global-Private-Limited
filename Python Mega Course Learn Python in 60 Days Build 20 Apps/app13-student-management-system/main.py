a2ce205dbccd7ab4b9470546e862f5a443198f3b
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-22 11:54:12 +0530

    Insertion, Searching, Table show

--- Python Mega Course Learn Python in 60 Days Build 20 Apps/app13-student-management-system/main.py
+++ Python Mega Course Learn Python in 60 Days Build 20 Apps/app13-student-management-system/main.py
@@ -1,62 +1,143 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
+     QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, \
+     QVBoxLayout, QComboBox
+from PyQt6.QtGui import QAction
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QComboBox, \
-    QGridLayout, QPushButton
+import sqlite3
 
 
-class SpeedCalculator(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Student Management System")
 
-        self.setWindowTitle('Average Speed Calculator')
-        grid = QGridLayout()
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+        edit_menu_item = self.menuBar().addMenu("&Edit")
 
-        # Create widgets
-        distance_label = QLabel('Distance:')
-        self.distance_input = QLineEdit()
+        add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
+        file_menu_item.addAction(add_student_action)
 
-        time_label = QLabel('Time (hours):')
-        self.time_input = QLineEdit()
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
 
-        self.unit_combo = QComboBox()
-        self.unit_combo.addItems(['Metric (km)', 'Imperial (miles)'])
+        search_action = QAction("Search", self)
+        edit_menu_item.addAction(search_action)
+        search_action.triggered.connect(self.search)
 
-        calculate_button = QPushButton('Calculate')
-        calculate_button.clicked.connect(self.calculate)
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
+        self.setCentralWidget(self.table)
 
-        self.result_label = QLabel("")
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
 
-        # Add widgets to grid
-        grid.addWidget(distance_label, 0, 0)
-        grid.addWidget(self.distance_input, 0, 1)
-        grid.addWidget(self.unit_combo, 0, 2)
-        grid.addWidget(time_label, 1, 0)
-        grid.addWidget(self.time_input, 1, 1)
-        grid.addWidget(calculate_button, 2, 1)
-        grid.addWidget(self.result_label, 3, 0, 1, 2)
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
 
-        self.setLayout(grid)
+    def search(self):
+        dialog = SearchDialog()
+        dialog.exec()
 
-    def calculate(self):
-        # Get distance and time from the input boxes
-        distance = float(self.distance_input.text())
-        time = float(self.time_input.text())
 
-        # Calculate average speed
-        speed = distance/time
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
 
-        # Check what user chose in the combo
-        if self.unit_combo.currentText() == 'Metric (km)':
-            speed = round(speed, 2)
-            unit = 'km/h'
-        if self.unit_combo.currentText() == 'Imperial (miles)':
-            speed = round(speed * 0.621371, 2)
-            unit = 'mph'
+        layout = QVBoxLayout()
 
-        # Display the result
-        self.result_label.setText(f"Average Speed: {speed} {unit}")
+        # Add student name widget
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # Add combo box of courses
+        self.course_name = QComboBox()
+        courses = ["Biology", "Math", "Astronomy", "Physics"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        # Add mobile widget
+        self.mobile = QLineEdit()
+        self.mobile.setPlaceholderText("Mobile")
+        layout.addWidget(self.mobile)
+
+        # Add a submit button
+        button = QPushButton("Register")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
+                       (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        main_window.load_data()
+
+
+class SearchDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        # Set window title and size
+        self.setWindowTitle("Search Student")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        # Create layout and input widget
+        layout = QVBoxLayout()
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # Create button
+        button = QPushButton("Search")
+        button.clicked.connect(self.search)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def search(self):
+        name = self.student_name.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT * FROM students WHERE name = ?", (name,))
+        row = list(result)[0]
+        print(row)
+        items = main_window.table.findItems("John Smith", Qt.MatchFlag.MatchFixedString)
+        for item in items:
+            print(item)
+            main_window.table.item(item.row(), 1).setSelected(True)
+
+        cursor.close()
+        connection.close()
 
 
 app = QApplication(sys.argv)
-calculator = SpeedCalculator()
-calculator.show()
+main_window = MainWindow()
+main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
