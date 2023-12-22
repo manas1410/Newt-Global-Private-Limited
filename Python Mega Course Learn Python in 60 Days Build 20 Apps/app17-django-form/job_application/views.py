48fb738d2140c508d290a1a6e55df490f597e5ea
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-22 17:09:49 +0530

    Nav bar and about use and admin created

--- Python Mega Course Learn Python in 60 Days Build 20 Apps/app17-django-form/job_application/views.py
+++ Python Mega Course Learn Python in 60 Days Build 20 Apps/app17-django-form/job_application/views.py
@@ -25,3 +25,6 @@ def index(request):
             messages.success(request, "Form Submitted Successfully!")
 
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
