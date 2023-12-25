53a3afeacd8dbaf880aa390bd0fa8a780593d15d
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-12-26 01:45:12 +0530

    app20-python-packages

---
+++ Python Mega Course Learn Python in 60 Days Build 20 Apps/app20-python-package/main.py
@@ -0,0 +1,6 @@
+from invoicing import invoice
+
+invoice.generate("invoices", "output", "pythonhow.png",
+                 "product_id","product_name",
+                 "amount_purchased","price_per_unit",
+                 "total_price")
