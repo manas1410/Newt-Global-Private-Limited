f61bf7971b094f49155f74b01e87384fccee0396
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2024-01-06 23:34:03 +0530

    Inheritance

---
+++ Java and Spring Framework for Beginners with Spring Boot/Inheritance.java
@@ -0,0 +1,8 @@
+public class Inheritance {
+    public static void main(String a[])
+    {
+        Calc obj = new Calc();
+        int r1 = obj.add(4, 5);
+        System.out.println(r1);
+    }
+}
